7d6035c8df8fabd62d8e6adacf13d170581f6e8f
Vanessa <vanessafsalinas@gmail.com>
2026-03-13 06:45:42 +0000

    finished assessData.cc

--- assessData.cc
+++ assessData.cc
@@ -17,6 +17,7 @@ void analyze(SleepData arr[], int count){
 
     cout << "\nStress And Sleep Levels:\n";
 
+
     for(int i = 0; i < count; i++){
         SleepData& d = arr[i];
 
@@ -35,6 +36,8 @@ void analyze(SleepData arr[], int count){
             sleep = "Good Sleep";
         }
 
+        //will output occupation, levels of stress and sleep
+        //shows correlation
         cout << d.occupation << ": " << stress << " + " << sleep << "\n";
 
     }
@@ -48,7 +51,7 @@ void process(SleepData arr[], int count){
     long long sleepQuality = 0;
     long long totalSleepScore = 0;
 
-    //heavy computation > this is what makes our program run so long
+    //heavy computation!!! > this is what makes our program run so long
     for(int year = 0; year < 1000; year++){
 
         for(int i = 0; i < count; i++){
@@ -66,15 +69,96 @@ void process(SleepData arr[], int count){
 
             double totalSleep = 0;
 
-            //calculates one year > 365 days of data
+            //calculates one year -> 365 days of data
             for(int day = 0; day < 365; day++){
-                
+                //variation in sleep needed b/c people dont get the same amount of sleep everyday
+                double currentSleep = baseSleep + ((day % 3) - 1) * 0.5;
+                //variation of 0.5 hours
+
+                //cant have negative sleeps
+                if(currentSleep < 0){
+                    currentSleep = 0;
+                }
+
+                //adds current sleep to the total amount of sleep so far
+                totalSleep += currentSleep;
+
+                // 7 hours is rec good, effective sleep
+                if(currentSleep < 7){
+                    //if less than 7 hrs sleep then they accumulate sleep debt
+                    //low sleep + high stress = more debt
+                    if(stressAmount > 5 ){
+                        sleepDebt += (7 - currentSleep) * 1.6;
+                        quality -= 0.4;
+                    } else {
+                        //low sleep + low stress = less debt
+                        sleepDebt += (7 - currentSleep);
+                        quality -= 0.2;
+                    }
+
+                } else {
+                    //more sleep + higher stress = less effective sleep
+                    if (stressAmount > 5){
+                        sleepDebt -= 0.1;
+                        quality += 0.1;
+                    } else {
+                        sleepDebt -= -0.3;
+                        quality += 0.3;
+                    }
+                }
+
+                //Physical activity and sleep relations
+                if(activity > 45){
+                    quality += 0.2;
+                    //more physical activity = higher quality sleep
+                } else if(activity < 15){
+                    quality -= 0.2; //less physical acvitiy = less quality sleep
+                }
+
+                //Stress and sleep relation
+                if (stressAmount > 5){
+                    deepSleep += currentSleep * 0.6;
+                } else {
+                    deepSleep += currentSleep * 0.85;
+                }
+
+                //these prevent impossible numbers
+                //cant have negative sleep debt, quality and more than 100 quality
+                if (sleepDebt < 0){
+                    sleepDebt = 0;
+                }
+
+                if(quality < 0){
+                    quality = 0;
+                }
+
+                if(quality > 100){
+                    quality = 100;
+                }
 
             }
+
+            //finds how effective sleep is overall
+            double effectiveSleep = deepSleep / totalSleep;
+
+            //getting totals after running computations
+            sleepDebtTotal += (long long)sleepDebt;
+            sleepQuality += (long long)quality;
+            totalSleepScore += (long long)(effectiveSleep * 1000);
 
         }
 
 
     }
+
+    cout << "\n--------------------------------------------------\n";
+    cout << "            Sleep and Lifestyle Findings            \n";
+    cout << "\n--------------------------------------------------\n";
+
+    cout << "Total Sleep Debt: \t" << sleepDebtTotal << "\n";
+    cout << "Total Sleep Quality: \t" << sleepQuality << "\n";
+    cout << "Total Restorative Sleep: \t" << totalSleepScore << "\n";
+
+
 
 }
